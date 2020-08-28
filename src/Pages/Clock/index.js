11934f1ac51315dca18a02f75b74eb38784572fe
Karol Bartyzel <karolbartyzel308@gmail.com>
2020-08-28 14:39:22 +0200

    Add clock subsite.

--- src/Pages/Clock/index.js
+++ src/Pages/Clock/index.js
@@ -1,7 +1,35 @@
 import React from 'react';
+import { IoMdGlobe } from 'react-icons/io';
+
+import './index.css';
 
 function Clock(props) {
-  return <div>Clock</div>;
+  const [now, setNow] = React.useState(new Date(Date.now()));
+
+  React.useEffect(() => {
+    const interval = window.setInterval(() => {
+      setNow(new Date(Date.now()));
+    }, 1000);
+
+    return () => {
+      window.clearInterval(interval);
+    }
+  }, []);
+
+  return (
+    <div className="Clock">
+      <div className="Clock-Timezone">
+      {`Timezone: GMT${now.getTimezoneOffset() > 0 ? '-' : '+'}${Math.floor(Math.abs(now.getTimezoneOffset()) / 60)}`}
+      </div>
+      <IoMdGlobe size={128} />
+      <div className="Clock-Time">
+      {now.toLocaleTimeString()}
+      </div>
+      <div className="Clock-Date">
+      {now.toLocaleDateString()}
+      </div>
+    </div>
+  );
 }
 
 export default Clock;
